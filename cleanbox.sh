5825bc75767247c1aa17d7680ebeea25aaf9440d
CarlosFernando <carlosfdsn2008@gmail.com>
2025-12-30 22:00:38 -0300

    Create cleanbox.sh

---
+++ cleanbox.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# ===== SETTINGS =====
+DAYS=160
+
+FOLDERS=(
+    "/storage/emulated/0/.Trash"
+    "/storage/emulated/0/Temp"
+    "/storage/emulated/0/tmp"
+    "/storage/emulated/0/Lost.dir"
+    "/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media"
+    "/storage/emulated/0/Download"
+)
+
+echo "Deleting files larger than $DIAS days..."
+echo "------------------------------------------"
+
+for FOLDER in "${FOLDERS[@]}"; do
+  if [ -d "$FOLDER" ]; then
+    echo "Deleting: $FOLDER"
+    find "$FOLDER" -type f -mtime +$DIAS -print -delete
+  else
+    echo "Folder not found: $FOLDER"
+  fi
+done
+
+echo "Done."
